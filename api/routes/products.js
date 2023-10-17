642e9579133c778a50fbad0b8e70b88c08a353cf
ISHITA AMOD <ishitaamod@gmail.com>
2023-10-17 11:09:36 +0530

    delete , add and get products

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,9 +4,15 @@ const Product = require("../models/products");
 const mongooose = require("mongoose");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Handling GET req to /products",
-  });
+ Product.find().exec().then( docs=>{
+    console.log(docs)
+    res.status(200).json(docs)
+ }).catch(err=>{
+    console.log(err)
+    res.status(500).json({
+        error: err
+    })
+ });
 });
 
 router.post("/", (req, res, next) => {
@@ -19,28 +25,38 @@ router.post("/", (req, res, next) => {
     .save()
     .then((result) => {
       console.log(result);
+      res.status(201).json({
+        message: "Handling POST req to /products",
+        createdProduct: result,
+      });
     })
     .catch((err) => {
       console.log(err);
+      res.status(500).json({
+        error: err,
+      });
     });
-  res.status(201).json({
-    message: "Handling POST req to /products",
-    createdProduct: product,
-  });
 });
 
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
-  .exec()
-  .then(doc => {
-    console.log(doc);
-    res.status(200).json({doc})
-  })
-  .catch(err => {
-    console.log(err)
-    res.status(500).json({error: err})
-  });
+    .exec()
+    .then((doc) => {
+      console.log(doc);
+      if (doc) {
+        res.status(200).json({ doc });
+      }
+      else{
+        res.status(404).json({
+            message: "No valid id found for product"
+        })
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.patch("/:productId", (req, res, next) => {
@@ -50,8 +66,14 @@ router.patch("/:productId", (req, res, next) => {
 });
 
 router.delete("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "deleted product!",
+    const id = req.params.productId
+  Product.deleteOne({_id: id}).exec().then(result=>{
+    res.status(200).json(result)
+  }).catch(err=>{
+    console.log(err)
+    res.status(500).json({
+        error:err
+    })
   });
 });
 
