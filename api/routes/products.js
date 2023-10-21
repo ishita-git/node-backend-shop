e6ffd57148eb302c575c1eadb7bca7ef46e1b551
ISHITA AMOD <ishitaamod@gmail.com>
2023-10-21 12:13:32 +0530

    patch request for updating

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,15 +4,24 @@ const Product = require("../models/products");
 const mongooose = require("mongoose");
 
 router.get("/", (req, res, next) => {
- Product.find().exec().then( docs=>{
-    console.log(docs)
-    res.status(200).json(docs)
- }).catch(err=>{
-    console.log(err)
-    res.status(500).json({
-        error: err
+  Product.find()
+    .exec()
+    .then((docs) => {
+      console.log(docs);
+      if (docs.length >= 0) {
+        res.status(200).json(docs);
+      } else {
+        res.status(404).json({
+          message: "No entries found",
+        });
+      }
     })
- });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
@@ -46,11 +55,10 @@ router.get("/:productId", (req, res, next) => {
       console.log(doc);
       if (doc) {
         res.status(200).json({ doc });
-      }
-      else{
+      } else {
         res.status(404).json({
-            message: "No valid id found for product"
-        })
+          message: "No valid id found for product",
+        });
       }
     })
     .catch((err) => {
@@ -60,21 +68,38 @@ router.get("/:productId", (req, res, next) => {
 });
 
 router.patch("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "Updated product!",
-  });
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.findOneAndUpdate({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result=>{
+        console.log(result)
+        res.status(200).json(result)
+    })
+    .catch(error=>{
+        console.log(error)
+        res.status(500).json({
+            error: error
+        })
+    });
 });
 
 router.delete("/:productId", (req, res, next) => {
-    const id = req.params.productId
-  Product.deleteOne({_id: id}).exec().then(result=>{
-    res.status(200).json(result)
-  }).catch(err=>{
-    console.log(err)
-    res.status(500).json({
-        error:err
+  const id = req.params.productId;
+  Product.deleteOne({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
     })
-  });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
